f556fe8843705e0d641b5468dfaeb15a94c9ead1
ThogNguyen <thoggnguyen2004@gmail.com>
2024-06-24 23:20:55 +0700

    dfdfsd

--- PS36400_NguyenLocThong_Lab3/Repositories/IProductService.cs
+++ PS36400_NguyenLocThong_Lab3/Repositories/IProductService.cs
@@ -6,12 +6,6 @@ namespace PS36400_NguyenLocThong_Lab3.Repositories
 	{
 		List<Product> GetAllProducts();
 
-        // Code chạy ok 
-        // Code chạy ok 2
-<<<<<<< HEAD
-        // Code đã fix
-=======
-        // Code lỗi
->>>>>>> ae2798940ab7de1cab1f0304878d58765a1e7f27
+        // Code chạy ok
     }
 }
